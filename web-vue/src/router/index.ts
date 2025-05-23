6d9d8f0318860a78f90192b5f6b676d1b13d06c5
sonic711 <uanex711@gmail.com>
2025-05-23 16:59:07 +0800

    router dix

--- web-vue/src/router/index.ts
+++ web-vue/src/router/index.ts
@@ -9,6 +9,10 @@ const router = createRouter({
       component: () => import('../views/HomePage.vue')
     },
     {
+      path: '',
+      redirect: ''
+    },
+    {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
